dc4b696d739d33c11e9932f4bc80bba392748288
blxtre <127733096+blxtre@users.noreply.github.com>
2023-03-26 13:28:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,34 @@
-# python3
+class PhoneBook:
+    def __init__(self):
+        self.contacts = {}
 
-class Query:
-    def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
-        if self.type == 'add':
-            self.name = query[2]
+    def add_contact(self, number, name):
+        self.contacts[number] = name
 
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    def delete_contact(self, number):
+        if number in self.contacts:
+            del self.contacts[number]
 
-def write_responses(result):
-    print('\n'.join(result))
+    def find_contact(self, number):
+        if number in self.contacts:
+            return self.contacts[number]
+        else:
+            return "not found"
 
 def process_queries(queries):
+    phone_book = PhoneBook()
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+    for query in queries:
+        if query[0] == 'add':
+            phone_book.add_contact(int(query[1]), query[2])
+        elif query[0] == 'del':
+            phone_book.delete_contact(int(query[1]))
+        elif query[0] == 'find':
+            result.append(phone_book.find_contact(int(query[1])))
     return result
 
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
-
+    n = int(input())
+    queries = [input().split() for i in range(n)]
+    result = process_queries(queries)
+    print('\n'.join(result))
